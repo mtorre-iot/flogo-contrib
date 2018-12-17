9d91dff0a9fcf318d95023cf800869f9bff5b5f6
mtorre-iot <mtorre.work@gmail.com>
2018-12-17 10:36:56 -0600

    small mod

--- trigger/mqtt/trigger.go
+++ trigger/mqtt/trigger.go
@@ -13,7 +13,7 @@ import (
 )
 
 // log is the default package logger
-var log = logger.GetLogger("trigger-flogo-mqtt")
+var log = logger.GetLogger("trigger-flogo-mqtt-mt")
 
 // MqttTrigger is simple MQTT trigger
 type MqttTrigger struct {
