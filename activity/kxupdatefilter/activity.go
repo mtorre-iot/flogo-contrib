d4356f1f379eb9a0609abf84f730a450b4161b58
mtorre-iot <mtorre.work@gmail.com>
2018-12-30 18:10:32 -0600

    KXlib separated

--- activity/kxupdatefilter/activity.go
+++ activity/kxupdatefilter/activity.go
@@ -3,8 +3,6 @@ package kxupdatefilter
 import (
 	"fmt"
 	"errors"
-	"time"
-	"encoding/json"
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
 )
@@ -64,74 +62,4 @@ func (a *KXUpdateFilterActivity) Eval(context activity.Context) (done bool, err 
 		context.SetOutput(ovMessage, message)
 	}
 	return true, nil
-}
-
-
-
-// Quality enum
-type Quality int
-
-const (
-	QualityOk	    	Quality = iota 
-	QualityOld   
-	QualityBad   
-	QualityUnknown
-)
-
-func (quality Quality) String() string {
-    names := [...]string{
-        "OK", 
-        "OLD", 
-        "BAD", 
-        "UNKNOWN"}
-    if quality < QualityOk || quality > QualityUnknown {
-      return "UNKNOWN"
-    }
-    return names[quality]
-}
-
-func GetQualityFromString(qualityStr string) (Quality, error) {
-	qual := map[string]Quality {
-		"OK": QualityOk,
-		"OLD": QualityOld,
-		"BAD": QualityBad,
-		"UNKNOWN": QualityUnknown }
-	rtn, ok := qual[qualityStr]
-	if (ok == false) {
-			return QualityUnknown, errors.New("Quality " + qualityStr + "is unkonwn")
-		}
-	return rtn, nil
-}
-// KXRTPObject configuration structure for Physical Objects
-type KXRTPObject struct {
-	ID int
-	Tag string
-	Ptype string
-	Cv *RtVal
-	Pv *RtVal
-	Avg *RtAvg
-}
-// RtVal represent a realtime sample
-type RtVal struct {
-	Value float64
-	ValueStr string
-	Quality Quality
-	Timestamp time.Time
-}
-// RtAvg represent Realtime calculated averages
-type RtAvg struct {
-	Average float64
-	Variance float64
-}
-
-// DecodeUpdateMessage get messages coming from a KXDataProc
-func DecodeUpdateMessage (message string) []KXRTPObject {
-
-	rtn := make([]KXRTPObject, 0)
-	var updateMessage []KXRTPObject 
-	// decode message
-	if err := json.Unmarshal([]byte(message), &updateMessage); err != nil {
-		return nil
-	}
-	return rtn
 }
