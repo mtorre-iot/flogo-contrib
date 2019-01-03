82a691982381fa8cb3785ffa97a74eab05837879
mtorre-iot <mtorre.work@gmail.com>
2019-01-03 13:15:04 -0600

    integrated debugging 1

--- activity/kxupdatefilter/activity.go
+++ activity/kxupdatefilter/activity.go
@@ -16,8 +16,7 @@ const (
 	ivInputTag1 = "inputTag1"
 	ivInputTag2 = "inputTag2"
 
-	ovOutput = "outputVal"
-	ovOutputTag1 = "outputTag1"
+	ovOutput = "outputStream"
 )
 
 func init() {
@@ -25,8 +24,6 @@ func init() {
 }
 
 // KXUpdateFilterActivity is an Activity that is used to deserialize messages from KXDataProc, to get changes to invoke other activities 
-// inputs : {message}
-// outputs: none
 type KXUpdateFilterActivity struct {
 	metadata *activity.Metadata
 }
@@ -48,7 +45,6 @@ func (a *KXUpdateFilterActivity) Eval(context activity.Context) (done bool, err 
 	triggerTag,_ := context.GetInput(ivTriggerTag).(string)
 	inputTag1,_ := context.GetInput(ivInputTag1).(string)
 	inputTag2,_ := context.GetInput(ivInputTag2).(string)
-	//outputTag1,_ := context.GetInput(ovOutputTag1).(string)
 
 	var input1Value float64 
 	var input2Value float64
@@ -56,7 +52,6 @@ func (a *KXUpdateFilterActivity) Eval(context activity.Context) (done bool, err 
 	var triggerObj KXRTPObject
 	var input1Obj KXRTPObject 
 	var input2Obj KXRTPObject 
-	//var output1Obj KXRTPObject
 	//
 	// decode it from Json
 	//
@@ -110,13 +105,6 @@ func (a *KXUpdateFilterActivity) Eval(context activity.Context) (done bool, err 
 			}
 		}
 		//
-		// Get the Output object
-		//
-/* 		output1Obj, err = GetRTPObject(db, outputTag1)
-		if (err != nil)	{
-			activityLog.Error(fmt.Sprintf("Tag: %s could not be accessed from Realtime Database. Error %s", outputTag1, err))
-		} */
-		//
 		// We should have the input values. Let's to create the output argument message
 		//
 		input1Value = input1Obj.Cv.Value
@@ -137,21 +125,6 @@ func (a *KXUpdateFilterActivity) Eval(context activity.Context) (done bool, err 
 		}
 		activityLog.Info(fmt.Sprintf("Output Message: %s", requestJson))
 		context.SetOutput(ovOutput, requestJson)
-/*
-		output1Value = input1Value + input2Value
-		//
-		// Create the json scan message back to KXDataproc
-		//
-		scanMessage := ScanMessageNew()
-		smu := ScanMessageUnitNew(output1Obj.ID, outputTag1, fmt.Sprintf("%f", output1Value), QualityOk.String(), MessageUnitTypeValue, time.Now().UTC())
-		scanMessage.ScanMessageAdd(smu)
-		jsonMessage, err := SerializeObject(scanMessage)
-		if err != nil {
-			activityLog.Error(fmt.Sprintf("Error trying to serialize output message. Error %s", err))
-			return false, err
-		}
-		activityLog.Info(fmt.Sprintf("Output Message: %s", jsonMessage))
-		context.SetOutput(ovOutput, jsonMessage) */
 	}
 	return foundTrig, nil
 }
