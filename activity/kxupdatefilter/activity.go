07fcf6001e3d1088a5ad1682a7bffba864757a4b
mtorre-iot <mtorre.work@gmail.com>
2019-01-03 16:50:03 -0600

    multiple inputs

--- activity/kxupdatefilter/activity.go
+++ activity/kxupdatefilter/activity.go
@@ -13,14 +13,14 @@ var activityLog = logger.GetLogger("activity-flogo-kxupdatefilter")
 const (
 	ivMessage   = "message"
 	ivTriggerTag = "triggerTag"
-	ivInputTag1 = "inputTag1"
-	ivInputTag2 = "inputTag2"
-
+//	ivInputTag1 = "inputTag1"
+//	ivInputTag2 = "inputTag2"
+	ivInputTags = "inputTags"
 	ovOutput = "outputStream"
 )
 
 func init() {
-	activityLog.SetLogLevel(logger.InfoLevel)
+	activityLog.SetLogLevel(logger.DebugLevel)
 }
 
 // KXUpdateFilterActivity is an Activity that is used to deserialize messages from KXDataProc, to get changes to invoke other activities 
@@ -43,15 +43,20 @@ func (a *KXUpdateFilterActivity) Eval(context activity.Context) (done bool, err 
 
 	message, _ := context.GetInput(ivMessage).(string)
 	triggerTag,_ := context.GetInput(ivTriggerTag).(string)
-	inputTag1,_ := context.GetInput(ivInputTag1).(string)
-	inputTag2,_ := context.GetInput(ivInputTag2).(string)
+	//inputTag1,_ := context.GetInput(ivInputTag1).(string)
+	//inputTag2,_ := context.GetInput(ivInputTag2).(string)
+	
+	val := context.GetInput(ivInputTags)
+	inputTags := val.(map[string]string)
 
-	var input1Value float64 
-	var input2Value float64
-	//var output1Value float64
+	//var input1Value float64 
+	//var input2Value float64
+	var inputValues map[string]float64
+
 	var triggerObj KXRTPObject
-	var input1Obj KXRTPObject 
-	var input2Obj KXRTPObject 
+	//var input1Obj KXRTPObject 
+	//var input2Obj KXRTPObject
+	var inputObjs map[string]KXRTPObject 
 	//
 	// decode it from Json
 	//
@@ -63,6 +68,14 @@ func (a *KXUpdateFilterActivity) Eval(context activity.Context) (done bool, err 
 	// test - print the tags
 	//
 	foundTrig:= false
+	inputValues = make(map[string]float64, len(inputTags))
+	inputObjs = make(map[string]KXRTPObject, len(inputTags))
+
+	for key := range inputTags {
+			inputObjs[key] = KXRTPObject{}
+			inputValues[key] = 0.0
+	}
+
 	for _, rtPObject := range decodedMessage {
 		// 
 		// Check if any of the received tags is the associated trigger
@@ -72,11 +85,17 @@ func (a *KXUpdateFilterActivity) Eval(context activity.Context) (done bool, err 
 			triggerObj = rtPObject
 			foundTrig = true
 		} 
-		if rtPObject.Tag == inputTag1 {
+/* 		if rtPObject.Tag == inputTag1 {
 			input1Obj = triggerObj
 		}
 		if triggerTag == inputTag2 {
 			input2Obj = triggerObj
+		} */
+
+		for key, intag := range inputTags {
+			if rtPObject.Tag == intag {
+				inputObjs[key] = triggerObj
+			}
 		}
 	}
 	if (foundTrig == true) {
@@ -92,7 +111,7 @@ func (a *KXUpdateFilterActivity) Eval(context activity.Context) (done bool, err 
 		// make sure it closes after finish
 		defer CloseRTDB(db)
 		// check all tags
-		if input1Obj.Tag == "" {
+/* 		if input1Obj.Tag == "" {
 			input1Obj, err = GetRTPObject(db, inputTag1)
 			if (err != nil)	{
 				activityLog.Error(fmt.Sprintf("Tag: %s could not be accessed from Realtime Database. Error %s", inputTag1, err))
@@ -103,18 +122,33 @@ func (a *KXUpdateFilterActivity) Eval(context activity.Context) (done bool, err 
 			if (err != nil)	{
 				activityLog.Error(fmt.Sprintf("Tag: %s could not be accessed from Realtime Database. Error %s", inputTag2, err))
 			}
+		} */
+		for key, pobj := range inputObjs {
+			if pobj.Tag == "" {
+				inputObjs[key], err = GetRTPObject(db, pobj.Tag)
+				if (err != nil)	{
+					activityLog.Error(fmt.Sprintf("Tag: %s could not be accessed from Realtime Database. Error %s", pobj.Tag, err))
+				}
+			}
 		}
 		//
 		// We should have the input values. Let's to create the output argument message
 		//
-		input1Value = input1Obj.Cv.Value
-		input2Value = input2Obj.Cv.Value
+		//input1Value = input1Obj.Cv.Value
+		//input2Value = input2Obj.Cv.Value
 		// {"func": "process1", "args":[{"name": "arg1","value": "1234", "quality": "OK"},{ "name": "arg2", "value":"222", "quality": "OK"}]}
 		// Create the request message
 		//
+		//args:= [] AnalyticsArg{}
+		//args = append(args, AnalyticsArgNew("arg1", fmt.Sprintf("%f", input1Value), input1Obj.Cv.Quality.String()))
+		//args = append(args, AnalyticsArgNew("arg2", fmt.Sprintf("%f", input2Value), input1Obj.Cv.Quality.String()))
+
 		args:= [] AnalyticsArg{}
-		args = append(args, AnalyticsArgNew("arg1", fmt.Sprintf("%f", input1Value), input1Obj.Cv.Quality.String()))
-		args = append(args, AnalyticsArgNew("arg2", fmt.Sprintf("%f", input2Value), input1Obj.Cv.Quality.String()))
+		cnt := 1
+		for _, pobj := range inputObjs {
+			args = append(args, AnalyticsArgNew(fmt.Sprintf("arg%d", cnt), fmt.Sprintf("%f", pobj.Cv.Value), pobj.Cv.Quality.String()))
+			cnt++
+		}
 
 		request := AnalyticsRequestNew("process1", args)
 
