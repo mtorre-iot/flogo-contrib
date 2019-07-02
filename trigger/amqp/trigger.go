c92f277acc8ef50bdf66087b15319ba68146b92b
mtorre-iot <mtorre.work@gmail.com>
2019-07-01 19:50:49 -0500

    keep fixing

--- trigger/amqp/trigger.go
+++ trigger/amqp/trigger.go
@@ -301,7 +301,7 @@ func (t *AmqpTrigger) Start() error {
 func (t *AmqpTrigger) receiverHandler(msgs <-chan amqp.Delivery) {
 	for d := range msgs {
 		payload := fmt.Sprintf("%s", d.Body)
-		log.Debugf("[amqp] Message received: %s", payload)
+		log.Infof("[amqp] Message received: %s", payload)
 		handler, found := t.topicToHandler[routingKey]
 		if found {
 			t.RunHandler(handler, payload)
