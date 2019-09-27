4e92496c0955143dd9634fb4e28791b374d96502
mtorre-iot <mtorre.work@gmail.com>
2019-09-26 22:06:07 -0500

    debugging

--- activity/kxcommon/kxtsdb.go
+++ activity/kxcommon/kxtsdb.go
@@ -82,7 +82,7 @@ func (tsdb *TSDB)  QueryTSOneTagTimeRange(database string, table string, tag str
 	queryStr2 = fmt.Sprintf(queryStr2, fieldStr, table)
 
 	query := influxdb.Query {
-		Command: queryStr,
+		Command: queryStr2,
 		Database: database,
 	}
 	resp, err := tsdb.connection.Query(query)
@@ -94,8 +94,7 @@ func (tsdb *TSDB)  QueryTSOneTagTimeRange(database string, table string, tag str
 	//
 	if (len(resp.Results) > 0) {
 		 // create an array of KXHistTSRecord out of the response
-		 //fmt.Println(resp.Results)
-	}
+		 }
 	// not found - return nil
 	return resp.Results, nil
 }
